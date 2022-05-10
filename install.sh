09b5729c907331ad5c4579af534b9f4f810aa6ab
Adam Aili <adam.aili@arriver.com>
2022-05-10 13:15:49 +0200

    Add default shell to tmux

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu pipefail
 
-SCRIPT_DIR="$(realpath $(dirname $0))"
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 
 ln -s "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$SCRIPT_DIR/.emacs" "$HOME/.emacs"
