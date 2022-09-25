837299dc563df9fcd4ef9d473adc938bff7973f5
Rasrack <karl.adam.aili@gmail.com>
2022-09-25 18:49:13 +0200

    Add git files

--- install.sh
+++ install.sh
@@ -2,37 +2,38 @@
 set -euo pipefail
 
 append_line() {
-  set -e
+    set +o pipefail
 
-  local update line file pat lno
-  update="$1"
-  line="$2"
-  file="$3"
-  pat="${4:-}"
-  lno=""
+    local update line file pat lno
+    update="$1"
+    line="$2"
+    file="$3"
+    pat="${4:-}"
+    lno=""
 
-  echo "Update $file:"
-  echo "  - $line"
-  if [ -f "$file" ]; then
-    if [ $# -lt 3 ]; then
-      lno=$(\grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
-    else
-      lno=$(\grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
+    echo "Update $file:"
+    echo "  - $line"
+    if [ -f "$file" ]; then
+        if [ $# -lt 3 ]; then
+            lno=$(\grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
+        else
+            lno=$(\grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
+        fi
     fi
-  fi
-  if [ -n "$lno" ]; then
-    echo "    - Already exists: line #$lno"
-  else
-    if [ $update -eq 1 ]; then
-      [ -f "$file" ] && echo >> "$file"
-      echo "$line" >> "$file"
-      echo "    + Added"
+    if [ -n "$lno" ]; then
+        echo "    - Already exists: line #$lno"
     else
-      echo "    ~ Skipped"
+        if [ "$update" -eq 1 ]; then
+            [ -f "$file" ] && echo >> "$file"
+            echo "$line" >> "$file"
+            echo "    + Added"
+        else
+            echo "    ~ Skipped"
+        fi
     fi
-  fi
-  echo
-  set +e
+    echo
+
+    set -o pipefail
 }
 
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
@@ -41,6 +42,8 @@ THIRDPARTY="${SCRIPT_DIR}/thirdparty"
 ln -s -f "${THIRDPARTY}/oh-my-tmux/.tmux.conf" "${HOME}/.tmux.conf"
 ln -s -f "${SCRIPT_DIR}/tmux.config.local" "${HOME}/.tmux.conf.local"
 ln -s -f "${SCRIPT_DIR}/emacs_config" "${HOME}/.emacs"
+ln -s -f "${SCRIPT_DIR}/gitignore" "$HOME/.gitignore"
+git config --global include.path "${SCRIPT_DIR}/gitconfig"
 
 BASHRC_WORKSPACE="${SCRIPT_DIR}/bashrc_workspace"
 append_line 1 "[ -f $BASHRC_WORKSPACE ] && source $BASHRC_WORKSPACE" "$HOME/.bashrc" "$BASHRC_WORKSPACE"
